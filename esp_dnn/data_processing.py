fcc056ac4c7059fe7b4fcc449a7dc173d0389177
urzone <github.com@urz.one>
2022-08-04 00:38:12 +0000

    Initial commit

---
+++ esp_dnn/data_processing.py
@@ -0,0 +1,71 @@
+# Copyright 2019 Astex Therapeutics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+import numpy as np
+import pickle
+
+
+def normalize(a, skip_norm_mask=None, params_pickle=None, params_dict=None):
+    """ normalizez a 3d array (mol, atom, props) in prop axis """
+
+    a_2d = np.reshape(a, (a.shape[0] * a.shape[1], a.shape[2]))
+    if params_pickle is not None:
+        with open(params_pickle) as f:
+            d = pickle.load(f)
+            mean = d["mean"]
+            std = d["std"]
+    elif params_dict is not None:
+        mean = params_dict["mean"]
+        std = params_dict["std"]
+    else:
+        mean = np.nanmean(a_2d, axis=0, keepdims=True)
+        std = np.nanmean(a_2d, axis=0, keepdims=True)
+
+    if skip_norm_mask is not None:
+        # do not standardized one-hot-encoded properties
+        skip_norm_mask = np.expand_dims(skip_norm_mask, 0)
+        # do not standardize all-zero props
+        skip_norm_mask |= (std == 0.0)
+
+        mean[skip_norm_mask] = 0.0
+        std[skip_norm_mask] = 1.0
+
+    return np.reshape((a_2d - mean) / std, a.shape), mean, std
+
+
+def ds_to_data(ds, normalize_x=True, norm_params_infile=None,
+               norm_param_outfile=None):
+    """ Converts xarray dataset to numpy arrays """
+    x = ds.X.values
+
+    train_mask = ds.train.values | ds.valid.values
+
+    if normalize_x:
+        skip_norm_mask = np.array([v.startswith("is_")
+                                   for v in ds.feature.values])
+        x, mean, std = normalize(
+            x, skip_norm_mask=skip_norm_mask, params_pickle=norm_params_infile)
+        if norm_param_outfile:
+            with open(norm_param_outfile, "wb") as f:
+                pickle.dump(dict(mean=mean, std=std), f)
+    x[np.isnan(x)] = 0.0
+
+    y = ds.Y.values
+
+    d = ds.D.values
+    d[np.isnan(d)] = 0.0
+
+    return x, d, y, train_mask
---
+++ esp_dnn/graph_conv.py
@@ -0,0 +1,158 @@
+# Copyright 2019 Astex Therapeutics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+from keras import activations, initializers, regularizers, constraints
+from keras import backend as K
+from keras.constraints import min_max_norm
+from keras.engine.topology import Layer, InputSpec
+import tensorflow as tf
+
+
+class PrintLayerInput(Layer):
+    def call(self, inputs):
+        inputs = tf.Print(inputs, data=[inputs],
+                          message="layer inputs: ", summarize=100)
+        return inputs
+
+
+class GraphConv(Layer):
+
+    def __init__(self,
+                 width,
+                 activation=None,
+                 use_bias=True,
+                 kernel_initializer="glorot_uniform",
+                 bias_initializer="zeros",
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 # "single": only one weight applied to all neighbor sums
+                 # "all": a different weight for each property
+                 conv_wts="single",
+                 **kwargs):
+
+        if "input_shape" not in kwargs and "input_dim" in kwargs:
+            kwargs["input_shape"] = (kwargs.pop("input_dim"),)
+
+        allowed_conv_wts = ("all", "single")
+        if conv_wts not in allowed_conv_wts:
+            raise ValueError("conv_wt should be one of %r" % allowed_conv_wts)
+
+        super(GraphConv, self).__init__(**kwargs)
+
+        self.width = width
+        self.conv_wts = conv_wts
+
+        self.activation = activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+        self.input_spec = [InputSpec(ndim=3), InputSpec(ndim=3)]
+
+    def build(self, input_shapes):
+        X_shape = input_shapes[0]
+        # number of atom props * output width
+        kernel_shape = (X_shape[-1], self.width)
+
+        # atom (self) weights
+        self.kernel_dense = self.add_weight(shape=kernel_shape,
+                                            initializer=self.kernel_initializer,
+                                            name="dense_kernel",
+                                            regularizer=self.kernel_regularizer,
+                                            constraint=self.kernel_constraint)
+        if self.use_bias is not None:
+            self.bias = self.add_weight(shape=(self.width,),
+                                        initializer=self.bias_initializer,
+                                        name="bias",
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+
+        constraint = min_max_norm(
+            min_value=0.0, max_value=1.0, rate=1.0, axis=0)
+        if self.conv_wts == "single":
+            self.kernel_neigh = self.add_weight(shape=[1],
+                                                initializer=self.kernel_initializer,
+                                                name="kernel_neigh",
+                                                regularizer=self.kernel_regularizer,
+                                                constraint=constraint)
+            self.kernel_self = self.add_weight(shape=[1],
+                                               initializer=self.kernel_initializer,
+                                               name="kernel_self",
+                                               regularizer=self.kernel_regularizer,
+                                               constraint=constraint)
+        elif self.conv_wts == "all":
+            self.kernel_neigh = self.add_weight(shape=(self.width,),
+                                                initializer=self.kernel_initializer,
+                                                name="kernel_neigh",
+                                                regularizer=self.kernel_regularizer,
+                                                constraint=constraint)
+            self.kernel_self = self.add_weight(shape=(self.width,),
+                                               initializer=self.kernel_initializer,
+                                               name="kernel_neigh",
+                                               regularizer=self.kernel_regularizer,
+                                               constraint=constraint)
+
+        self.built = True
+
+    def call(self, inputs):
+        x = inputs[0]  # n_atom * n_props
+        d = inputs[1]  # [n_atoms, n_atoms]
+
+        self_output = K.dot(x, self.kernel_dense)
+        # sum values from the neighbors
+        neigh_output = K.batch_dot(d, self_output, axes=[2, 1])
+
+        if self.conv_wts == "single":
+            neigh_output = neigh_output * self.kernel_neigh[0]
+            self_output = self_output * self.kernel_self[0]
+        elif self.conv_wts == "all":
+            neigh_output = neigh_output * self.kernel_neigh
+            self_output = self_output * self.kernel_self
+
+        output = self_output + neigh_output
+        if self.use_bias is not None:
+            output += K.reshape(self.bias, (1, self.width))
+        output = self.activation(output)
+        return output
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0][0], input_shape[0][1], self.width)
+
+    def get_config(self):
+        config = {
+            "width": self.width,
+            "conv_wts": self.conv_wts,
+            "activation": activations.serialize(self.activation),
+            "use_bias": self.use_bias,
+            "kernel_initializer": initializers.serialize(self.kernel_initializer),
+            "bias_initializer": initializers.serialize(self.bias_initializer),
+            "kernel_regularizer": regularizers.serialize(self.kernel_regularizer),
+            "bias_regularizer": regularizers.serialize(self.bias_regularizer),
+            "activity_regularizer": regularizers.serialize(self.activity_regularizer),
+            "kernel_constraint": constraints.serialize(self.kernel_constraint),
+            "bias_constraint": constraints.serialize(self.bias_constraint)
+        }
+        base_config = super(GraphConv, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
